cb20ca4e2bd6ff26943d43de082abc3e9b8fd325
Niki.Kalwar <niki@credibled.com>
2022-12-02 15:31:19 +0530

    arrow functions

--- app.js
+++ app.js
@@ -1,11 +1,2 @@
-console.log("app is running");
-
-let arrayIntegers=[1,2,3,4,5];
-let split=arrayIntegers.slice(1,3);
-console.log(split);
-console.log(arrayIntegers);
-
-let splice=arrayIntegers.splice(1,3)
-console.log(arrayIntegers);
-console.log(splice)
-//console.table([{"id":"30232"},{"id":"30234"}])
+//arrow functions are shorthand for functions which dont have this, arguments,super, or new.target
+//they cannot be used as constructors
