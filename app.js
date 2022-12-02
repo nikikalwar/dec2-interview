9f4f90b6a87c5e464d9e321770f28194f2eb1644
Niki.Kalwar <niki@credibled.com>
2022-12-02 15:28:47 +0530

    slice and splice

---
+++ app.js
@@ -0,0 +1,11 @@
+console.log("app is running");
+
+let arrayIntegers=[1,2,3,4,5];
+let split=arrayIntegers.slice(1,3);
+console.log(split);
+console.log(arrayIntegers);
+
+let splice=arrayIntegers.splice(1,3)
+console.log(arrayIntegers);
+console.log(splice)
+//console.table([{"id":"30232"},{"id":"30234"}])
